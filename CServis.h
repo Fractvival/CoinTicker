f7a039983decd79b8f10f521003d737755e7e252
Pajoš <fractvival@gmail.com>
2018-06-21 19:04:21 +0200

    Add files via upload

--- CServis.h
+++ CServis.h
@@ -2,14 +2,22 @@
 // Jadro Tickeru v1.0
 // * 04/2018
 //
+// NUTNO pridat ve spravci knihoven externi hnihovnu ArduinoJson 5.13.2 by Benoit Blanchon: 
+// https://github.com/bblanchon/ArduinoJson
+//
+/////////
+
 #include <EEPROM.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 // Velikost ROM v bajtech
 #define ROM_SIZE 512
 
-// Objekt pro obsluhu servere
+// Objekt pro obsluhu serveru na tickeru
 ESP8266WebServer server( 80 );
-// IP adresa serveru
+// IP adresa serveru na tickeru
 IPAddress myIP;
 
 
@@ -32,13 +40,26 @@ class CServis
   public:
 
     // NUTNO provest inicializaci pri setupu!
-    void Init();
+    void Init(bool SaveEprom);
     // TRUE pokud nazev wifi neni v tovarnim nastaveni
     bool IsSet();
     // Mod nastaveni tickeru, vraci cislo modu do ktereho ma byt ticker prepnuty
     void SettingMode();
+    // Vrati textovou podobu webove stranky pro nastaveni tickeru
     String GetStrPage();
-    void CServis::SaveData(int cArg, String Data);
+    // Uklada uzivatelem nove nebo zmenene data
+    // cArg je cislo textoveho pole, pocitano od 0
+    // cArg 0 = Prvni editacni pole na strance nastaveni
+    void SaveData(int cArg, String Data);
+    // Mod pripojeni tickeru k wifi
+    // Argument je pocet pokusu k pripojeni po sekundach
+    // Vraci TRUE v pripade uspechu
+    bool ConnectMode(int cAttempts);
+    // Nacteni dat o menach z netu
+    // Vraci TRUE v pripade uspechu
+    // Prvni argument je pocet pokusu na cteni po sekundach
+    // Druhy argument je cislo konecne URL API, pocitane od 0 do 3 (EndAPI=4)
+    bool ReadDataFromSite(int cAttemts, int endAPI );
 
 
   private:
@@ -78,8 +99,8 @@ class CServis
     String    __HDay;
     String    __HWeek;
 
-    // Obsah webove stranky pro nastaveni WiFi parametru
-    String    __strPage ="<HTML> <HEAD> <META NAME=\"GENERATOR\" CONTENT=\"PROGMaxi software\"> <META NAME=\"AUTHOR\" CONTENT=\"PROGMaxi software\"> <META HTTP-EQUIV=\"CONTENT-TYPE\" CONTENT=\"text/html; charset=Windows-1250\"> <TITLE>Ticker Settings</TITLE> </HEAD> <BODY TEXT=\"#000000\" LINK=\"#0000FF\" ALINK=\"#FF0000\" VLINK=\"#0000FF\" BGCOLOR=\"#C0C0C0\"> <H3 ALIGN=\"LEFT\">Welcome to Ticker settings</H3> <H4 ALIGN=\"LEFT\"><I></I><FONT COLOR=\"#FF0000\">*unchanged fields leaves blank</FONT></I></H4> <FORM METHOD=\"GET\"> <LABEL>SSID WIFI</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"ssid\" MAXLENGTH=32 SIZE=35><BR> <LABEL>PASSWORD WIFI</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"pass\" MAXLENGTH=32 SIZE=35><BR> <BR> <LABEL>API URL (e.g.: api.coinmarketcap.com)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"apiurl\" MAXLENGTH=62 SIZE=65><BR> <BR> <H4 ALIGN=\"LEFT\"><I></I><FONT COLOR=\"#FF0000\">**do not forget to attach the necessary JSON symbols</FONT></I></H4> <LABEL>COIN JSON SYMBOL<BR> (e.g.: \"symbol\")</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"coinsymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>PRICE JSON SYMBOL<BR> (e.g.: \"price\" or \"price_usd\")</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"pricesymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>HISTORY HOUR JSON SYMBOL<BR> (e.g.: \"percent_change_1h\")</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"hoursymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>HISTORY DAY JSON SYMBOL<BR> (e.g.: \"percent_change_24h\")</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"daysymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>HISTORY WEEK JSON SYMBOL<BR> (e.g.: \"percent_change_7d\")</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"weeksymbol\" MAXLENGTH=24 SIZE=27><BR> <BR> <LABEL>END URL FOR COIN 1<BR> (e.g.: /v1/ticker/bitcoin/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl1\" MAXLENGTH=62 SIZE=65><BR> <LABEL>END URL FOR COIN 2<BR> (e.g.: /v1/ticker/ethereum/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl2\" MAXLENGTH=62 SIZE=65><BR> <LABEL>END URL FOR COIN 3<BR> (e.g.: /v1/ticker/ripple/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl3\" MAXLENGTH=62 SIZE=65><BR> <LABEL>END URL FOR COIN 4<BR> (e.g.: /v1/ticker/iota/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl4\" MAXLENGTH=62 SIZE=65><BR> <BR> <INPUT TYPE=\"SUBMIT\" NAME=\"END\" VALUE=\"Save&Restart\" SIZE=15> <BR> <BR> </FORM> </BODY> </HTML>";
+    // Obsah webove stranky pro nastaveni WiFi tickeru
+    String    __strPage ="<HTML> <HEAD> <META NAME=\"GENERATOR\" CONTENT=\"PROGMaxi software\"> <META NAME=\"AUTHOR\" CONTENT=\"PROGMaxi software\"> <META HTTP-EQUIV=\"CONTENT-TYPE\" CONTENT=\"text/html; charset=Windows-1250\"> <TITLE>Ticker Settings</TITLE> </HEAD> <BODY TEXT=\"#000000\" LINK=\"#0000FF\" ALINK=\"#FF0000\" VLINK=\"#0000FF\" BGCOLOR=\"#C0C0C0\"> <H3 ALIGN=\"LEFT\">Welcome to Ticker settings</H3> <H4 ALIGN=\"LEFT\"><I></I><FONT COLOR=\"#FF0000\">*unchanged fields leaves blank</FONT></I></H4> <FORM METHOD=\"GET\"> <LABEL>SSID WIFI</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"ssid\" MAXLENGTH=32 SIZE=35><BR> <LABEL>PASSWORD WIFI</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"pass\" MAXLENGTH=32 SIZE=35><BR> <BR> <LABEL>API URL (e.g.: api.coinmarketcap.com)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"apiurl\" MAXLENGTH=62 SIZE=65><BR> <BR> <H4 ALIGN=\"LEFT\"><I></I><FONT COLOR=\"#FF0000\">**do not forget to attach the necessary JSON symbols</FONT></I></H4> <LABEL>COIN JSON SYMBOL<BR> (e.g.: symbol)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"coinsymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>PRICE JSON SYMBOL<BR> (e.g.: price or price_usd)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"pricesymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>HISTORY HOUR JSON SYMBOL<BR> (e.g.: percent_change_1h)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"hoursymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>HISTORY DAY JSON SYMBOL<BR> (e.g.: percent_change_24h)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"daysymbol\" MAXLENGTH=24 SIZE=27><BR> <LABEL>HISTORY WEEK JSON SYMBOL<BR> (e.g.: percent_change_7d)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"weeksymbol\" MAXLENGTH=24 SIZE=27><BR> <BR> <LABEL>END URL FOR COIN 1<BR> (e.g.: /v1/ticker/bitcoin/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl1\" MAXLENGTH=62 SIZE=65><BR> <LABEL>END URL FOR COIN 2<BR> (e.g.: /v1/ticker/ethereum/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl2\" MAXLENGTH=62 SIZE=65><BR> <LABEL>END URL FOR COIN 3<BR> (e.g.: /v1/ticker/ripple/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl3\" MAXLENGTH=62 SIZE=65><BR> <LABEL>END URL FOR COIN 4<BR> (e.g.: /v1/ticker/iota/)</LABEL><BR> <INPUT TYPE=\"TEXT\" NAME=\"endurl4\" MAXLENGTH=62 SIZE=65><BR> <BR> <INPUT TYPE=\"SUBMIT\" NAME=\"END\" VALUE=\"Save&Restart\" SIZE=15> <BR> <BR> </FORM> </BODY> </HTML>";
 
 };
 
@@ -89,24 +110,77 @@ String CServis::GetStrPage()
   return __strPage;
 }
 
+bool CServis::ReadDataFromSite(int cAttemts, int endAPI)
+{
+  const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(15) + 500;
+  DynamicJsonBuffer jsonBuffer(bufferSize);
+  //ArduinoJson::StaticJsonBuffer<bufferSize> jsonBuffer; 
+
+
+  JsonArray& root = jsonBuffer.parseArray(json);
+  JsonObject& root_0 = root[0];
+  const char* root_0_symbol = root_0[__Symbol];
+  const char* root_0_price_usd = root_0[__Price];
+  const char* root_0_percent_change_1h = root_0[__HHour];
+  const char* root_0_percent_change_24h = root_0[__HDay];
+  const char* root_0_percent_change_7d = root_0[__HWeek];
+
+  return true;
+}
+
+
+bool CServis::ConnectMode(int cAttempts)
+{
+  WiFi.mode(WIFI_STA);
+  Serial.println("* Ticker switched to WIFI_STA");
+  Serial.print("* Start connecting to wifi: ");
+  Serial.print(__ssidWifi);
+  Serial.print(" [");
+  Serial.print(__passWifi);
+  Serial.println("]");
+
+  Serial.print("* Connecting.");
+  int cLoop = 0;
+
+  WiFi.begin(__ssidWifi.c_str(),__passWifi.c_str());
+  while (WiFi.status() != WL_CONNECTED) 
+  { 
+    delay(1000);
+    Serial.print(".");
+    cLoop++;
+    if ( cLoop >= cAttempts )
+    {
+      return false;
+    }
+  } 
+
+  Serial.println("\n* Connected.");
+  Serial.println("* Now, attempt to first retrieve data from the site.");
+
+  Serial.print("* Reading.");
+  
+  return true;
+}
+
+
 void CServis::SettingMode()
 {
-  WiFi.mode(WIFI_AP_STA);
-  Serial.println("[S] Ticker switched to WIFI_AP_STA");
+  WiFi.mode(WIFI_AP);
+  Serial.println("* Ticker switched to WIFI_AP");
   WiFi.softAP(__ssidTicker.c_str(),__passTicker.c_str());
   IPAddress myIP = WiFi.softAPIP();  
-  Serial.print("[S] Ticker IP: ");
+  Serial.print("* Ticker IP: ");
   Serial.println(myIP);
-  Serial.print("[S] Ticker name: ");
+  Serial.print("* Ticker name: ");
   Serial.println(__ssidTicker);
-  Serial.print("[S] Ticker pass: ");
+  Serial.print("* Ticker pass: ");
   Serial.println(__passTicker);
 }
 
 
 bool CServis::IsSet()
 {
-  if ( __ssidWifi.equals("MyWifi") && __passWifi.equals("121212121212") )
+  if ( __ssidWifi.equals("MyWifi") )
   {
     return false;     
   }
@@ -152,21 +226,20 @@ String CServis::ReadEprom(int zbAddress, int iSize)
 }
 
 // Tato fce nacte data ulozene v epromce
-// Zakomentovana cast lze pouzit pro overeni nactenych dat
 void CServis::ReadEprom()
 {
-  String buffWifiName = ReadEprom(0,32);
-  String buffWifiPass = ReadEprom(32,32);
-  String buffAPIURL = ReadEprom(64,62);
-  String buffEndURL1 = ReadEprom(126,62);
-  String buffEndURL2 = ReadEprom(188,62);
-  String buffEndURL3 = ReadEprom(250,62);
-  String buffEndURL4 = ReadEprom(312,62);
-  String buffSymbol = ReadEprom(374,24);
-  String buffPrice = ReadEprom(398,24);
-  String buffHHour = ReadEprom(422,24);
-  String buffHDay = ReadEprom(446,24);
-  String buffHWeek = ReadEprom(470,24);
+  __ssidWifi = ReadEprom(0,32);
+  __passWifi = ReadEprom(32,32);
+  __API = ReadEprom(64,62);
+  __EndAPI[0] = ReadEprom(126,62);
+  __EndAPI[1] = ReadEprom(188,62);
+  __EndAPI[2] = ReadEprom(250,62);
+  __EndAPI[3] = ReadEprom(312,62);
+  __Symbol = ReadEprom(374,24);
+  __Price = ReadEprom(398,24);
+  __HHour = ReadEprom(422,24);
+  __HDay = ReadEprom(446,24);
+  __HWeek = ReadEprom(470,24);
 }
 
 
@@ -220,60 +293,61 @@ void CServis::SaveData(int cArg, String Data)
     }
     case 2:
     {
-      WriteEprom(64,62,__API);
-      break;
-    }
-    case 3:
-    {
-      WriteEprom(126,62,__EndAPI[0]);
-      break;
-    }
-    case 4:
-    {
-      WriteEprom(188,62,__EndAPI[1]);
-      break;
-    }
-    case 5:
-    {
-      WriteEprom(250,62,__EndAPI[2]);
-      break;
-    }
-    case 6:
-    {
-      WriteEprom(312,62,__EndAPI[3]);
-      break;
-    }
-    case 7:
-    {
-      WriteEprom(374,24,__Symbol);
+      __API = Data;
       break;
     }
     case 8:
     {
-      WriteEprom(398,24,__Price);
+      __EndAPI[0] = Data;
       break;
     }
     case 9:
     {
-      WriteEprom(422,24,__HHour);
+      __EndAPI[1] = Data;
       break;
     }
     case 10:
     {
-      WriteEprom(446,24,__HDay);
+      __EndAPI[2] = Data;
       break;
     }
     case 11:
     {
-      WriteEprom(470,24,__HWeek);
+      __EndAPI[3] = Data;
+      break;
+    }
+    case 3:
+    {
+      __Symbol = Data;
+      break;
+    }
+    case 4:
+    {
+      __Price = Data;
+      break;
+    }
+    case 5:
+    {
+      __HHour = Data;
+      break;
+    }
+    case 6:
+    {
+      __HDay = Data;
+      break;
+    }
+    case 7:
+    {
+      __HWeek = Data;
       break;
     }
   }
+  WriteEprom();
 }
 
 
 
-void CServis::Init()
+void CServis::Init( bool SaveEprom )
 {
   __ssidWifi = "MyWifi";
   __passWifi = "121212121212";
@@ -288,11 +362,11 @@ void CServis::Init()
   __EndAPI[2] = "/v1/ticker/ripple/";
   __EndAPI[3] = "/v1/ticker/iota/";
 
-  __Symbol = "\"symbol\"";
-  __Price = "\"price_usd\"";
-  __HHour = "\"percent_change_1h\"";
-  __HDay = "\"percent_change_24h\"";
-  __HWeek = "\"percent_change_7d\"";
+  __Symbol = "symbol";
+  __Price = "price_usd";
+  __HHour = "percent_change_1h";
+  __HDay = "percent_change_24h";
+  __HWeek = "percent_change_7d";
   
   __sCoin[0].ID = "BTC";
   __sCoin[1].ID = "ETH";
@@ -306,9 +380,12 @@ void CServis::Init()
       __sCoin[i].History[j] = 0;
     }
   }
-  
+  if ( SaveEprom )
+  {
+    Serial.println("* WRITE DEFAULT VALUES TO EPROM !");
+    WriteEprom();
+  }
   ReadEprom();
-  //WriteEprom();
 };
 
 
--- Ticky.ino
+++ Ticky.ino
@@ -1,31 +1,47 @@
-//#include <WiFi.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+// COIN TICKER v1.0
+// PROGMaxi software
+// 
+// Na serial portu probihaji vypisy danych akci o rychlosti 9600
+// PROSIM dodrzet tuto rychlost a pokud pribude zmena u ktere lze
+// provadet textovou kontrolu, zaved jeji vypis na konzolu
 
+// Knihovna obsahujici obsluznou tridu CServis
 #include "CServis.h"
 
+// Definice objektu tridy jadra CServis
 CServis servis;
+
+// Mody Tickeru -> nastavuje Ticker
+// 0 = Setting mode, nastaveni tickeru
+// 1 = Connect mode, pripojeni k Wifi
+// 2 = Normal mode, nacitani dat z netu a vypis na LCD
 int iMode;
 
-
+// Obsluha pro vypis webove stranky k nastaveni tickeru 
 void handleRoot()
 {
   server.send ( 200, "text/html", servis.GetStrPage() );
-  Serial.println(">> client refresh page!");
+  Serial.println("* Client refresh page!");
 }
 
-
+///////////////////////////////
+///////////////////////////////
 void setup() 
 {
-  Serial.begin(115200);
+  // Init konzoly
+  Serial.begin(9600);
   Serial.println("\n\n* CoinTicker v1.0");
-  servis.Init();
+  // Init jadra tickeru
+  // Argument TRUE provede zapis defaultnich hodnot pred ctenim dat z epromky
+  servis.Init(FALSE);
   Serial.println("* Init done!");
   Serial.println("* Checking ROM data for connecting to WiFi.");
-
+  // Kontrola zda/li je uz nastavena Wifi
+  // Pokud ne, spusti se Setting mode
+  // Pokud ano, spusti se mod pripojeni k Wifi
   if ( !servis.IsSet() )
   {
-    Serial.println("* Wifi is not set! Switching to setting mode.");
+    Serial.println("* Wifi is NOT set! Switching to setting mode.");
     Serial.println("* Setting mode starting.");
     servis.SettingMode();
     server.on ( "/", handleRoot );
@@ -34,14 +50,15 @@ void setup()
   }
   else
   {
-    Serial.println("* Wifi is set, start connecting to wifi.");
+    Serial.println("* Wifi IS set! Switching to connect mode.");
     iMode = 1;
   }
 }
 
+////////////////////////////////////
+////////////////////////////////////
 void loop() 
 {
-
   // 0 = Setting mode
   // 1 = Connect mode
   // 2 = Normal mode
@@ -50,20 +67,38 @@ void loop()
     case 0:
     {
       server.handleClient();
+      // Pokud bude nejaky argument ze strany klienta vyplnen a potvrzen, provedeme jeho zapis 
       if ( server.args() )
       {
         String message;
-        message += server.arg(0) + "\n";
-        message += server.arg(1) + "\n";
-        Serial.println(message);
+        for ( int i = 0; i < 12; i++ )
+        {
+          message = server.arg(i);
+          if ( message.length() > 0 )
+          {
+            servis.SaveData(i,message);
+            Serial.print("* Argument c.");
+            Serial.print(i);
+            Serial.print(" zmenen na: ");
+            Serial.println(message);
+          }
+        }
+        Serial.println("* ALL setting done, switching to Connect mode.");
         iMode = 1;
       }
       break;
     }
     case 1:
     {
-      
-
+      Serial.println("* Connect mode starting.");
+      // Pokud se nelze pripojit k wifi, proved dalsi kolo...
+      if ( !servis.ConnectMode(30) )
+      {
+        Serial.println("\n* Connection to wifi failed! We'll try again...");
+        break;
+      }
+      Serial.println("* Connect mode done, switching to Normal mode.");
+      iMode = 2;
       break;
     }
     case 2:
